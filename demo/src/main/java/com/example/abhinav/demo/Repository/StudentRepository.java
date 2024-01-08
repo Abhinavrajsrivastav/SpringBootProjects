927c745ae44ae30432441aa6bce29b440ee3ad68
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-01-08 15:59:15 +0530

    08-01-2023-001

--- demo/src/main/java/com/example/abhinav/demo/Repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.abhinav.demo.Repository;
-
-import com.example.abhinav.demo.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-    // Custom query methods if needed
-}
