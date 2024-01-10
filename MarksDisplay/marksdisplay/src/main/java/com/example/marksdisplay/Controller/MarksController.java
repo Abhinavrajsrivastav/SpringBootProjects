b368b7af7198be1e301ba40d29ebe9ad6204a70d
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-01-11 00:12:04 +0530

    11-01-2024-001

--- MarksDisplay/marksdisplay/src/main/java/com/example/marksdisplay/Controller/MarksController.java
+++ MarksDisplay/marksdisplay/src/main/java/com/example/marksdisplay/Controller/MarksController.java
@@ -1,6 +1,10 @@
 package com.example.marksdisplay.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +13,7 @@ import com.example.marksdisplay.Model.StudentMarks;
 import com.example.marksdisplay.Service.MarksService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class MarksController {
     
 
@@ -18,5 +23,10 @@ public class MarksController {
     @PostMapping("/")
     public StudentMarks save(@RequestBody StudentMarks studentMarks) {
         return marksService.save(studentMarks);
+    }
+
+    @GetMapping("/")
+    public List<StudentMarks> getAll() {
+        return marksService.getAll();
     }
 }
--- MarksDisplay/marksdisplay/src/main/java/com/example/marksdisplay/Service/MarksService.java
+++ MarksDisplay/marksdisplay/src/main/java/com/example/marksdisplay/Service/MarksService.java
@@ -1,20 +1,24 @@
 package com.example.marksdisplay.Service;
 
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.marksdisplay.Model.StudentMarks;
 import com.example.marksdisplay.Repository.MarksRepo;
 
+import java.util.List; // Correct import for List
+
 @Service
 public class MarksService {
-    
 
     @Autowired
     MarksRepo marksRepo;
 
     public StudentMarks save(StudentMarks studentMarks) {
         return marksRepo.save(studentMarks);
+    }
+
+    public List<StudentMarks> getAll() {
+        return marksRepo.findAll();
     }
 }
