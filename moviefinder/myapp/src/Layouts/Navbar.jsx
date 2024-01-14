9f03a3255dddc656c4199bcfdada2bd32b950aa7
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-01-14 13:16:16 +0530

    14-01-2024-001

--- moviefinder/myapp/src/Layouts/Navbar.jsx
+++ moviefinder/myapp/src/Layouts/Navbar.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import { } from 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 const Navbar = () => {
   return (
     <div className="box">
         <nav className="navbar navbar-expand-lg navbar-light bg-success px-2">
+    
   <div className="container-fluid">
+    <div className="brand">
+      <div className="brand-box">
     <Link className="navbar-brand" to="/">Movie finder</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+      <i className="bi bi-person-hearts fs-3 text-white"></i>
+    </div>
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
@@ -22,6 +25,7 @@ const Navbar = () => {
           <Link className="nav-link active" to="/Search">Search Movies</Link>
         </li>
       </ul>
+    </div>
     </div>
   </div>
   <div className="acknowledge">
