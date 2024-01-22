75f330d97bc28cc8e5575cd13f36c8f44bd3237b
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-01-22 21:27:53 +0530

    22-01-2024-001

---
+++ OpenAi/GptIntegration/demo/src/main/java/com/example/Controller/CustomBotController.java
@@ -0,0 +1,33 @@
+
+package com.example.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.dto.ChatGPTRequest;
+import com.example.demo.dto.ChatGPTResponse;
+
+import lombok.Value;
+
+@RestController
+@RequestMapping("/bot")
+public class CustomBotController{
+ 
+    
+    private String model = "gpt-3.5-turbo";
+    private String apiURL = "https://api.openai.com/v1/completions";
+
+    @Autowired
+    private RestTemplate template;
+
+    @GetMapping("/chat")
+    public String chat(@RequestParam("prompt") String prompt){
+        ChatGPTRequest request=new ChatGPTRequest(model, prompt);
+        ChatGPTResponse chatGptResponse = template.postForObject(apiURL, request, ChatGPTResponse.class);
+        return chatGptResponse.getChoices().get(0).getMessage().getContent();
+    }
+}
